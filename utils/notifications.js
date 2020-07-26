48c276fb522d008b9ee6e232b738cfe46ef0ad13
Alexandru Cotin <a.cotin96@gmail.com>
2020-07-26 16:46:17 +0200

    Prototype notifications currently not working due to permissions not updating

--- utils/notifications.js
+++ utils/notifications.js
@@ -3,7 +3,7 @@ import * as Notifications from "expo-notifications";
 
 import * as Permissions from "expo-permissions";
 
-const NOTIFICATION_KEY = "UdaciFitness:notifications";
+const NOTIFICATION_KEY = "Flashcards:notifications";
 
 export function clearLocalNotification() {
   return AsyncStorage.removeItem(NOTIFICATION_KEY).then(
@@ -13,8 +13,8 @@ export function clearLocalNotification() {
 
 function createNotification() {
   return {
-    title: "Log your stats!",
-    body: "👋 don't forget to log your stats for today!",
+    title: "Take a quiz!",
+    body: "👋 don't forget to study today!",
     ios: {
       sound: true,
     },
@@ -30,22 +30,22 @@ function createNotification() {
 export function setLocalNotification() {
   AsyncStorage.getItem(NOTIFICATION_KEY)
     .then(JSON.parse)
-    .then((data) => {
+    .then(async (data) => {
+      console.log("Notification key data: ", data);
       if (data === null) {
         Permissions.askAsync(Permissions.NOTIFICATIONS).then(({ status }) => {
+          // For some strange reason the status is always undetermined
+          // This notification method doesn't seems to work on iOS simulator
           if (status === "granted") {
             Notifications.cancelAllScheduledNotificationsAsync();
-
             let tomorrow = new Date();
-            tomorrow.setDate(tomorrow.getDate() + 1);
+            tomorrow.setDate(tomorrow.getDate());
             tomorrow.setHours(20);
             tomorrow.setMinutes(0);
-
             Notifications.scheduleLocalNotificationAsync(createNotification(), {
               time: tomorrow,
               repeat: "day",
             });
-
             AsyncStorage.setItem(NOTIFICATION_KEY, JSON.stringify(true));
           }
         });
